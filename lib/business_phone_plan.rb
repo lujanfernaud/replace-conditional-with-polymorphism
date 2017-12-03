dcf0ae7fdcedcdc1ec35517611877c5833454d94
lujanfernaud <lujanfernaud@gmail.com>
2017-12-03 15:51:30 +0000

    Extract #subtotal method in BusinessPhonePlan

--- lib/business_phone_plan.rb
+++ lib/business_phone_plan.rb
@@ -9,12 +9,16 @@ class BusinessPhonePlan
   end
 
   def cost
-    subtotal = @number_of_phones * @price
-
     if @number_of_phones < 50
       subtotal * 0.75
     else
       subtotal * 0.50
     end
+  end
+
+  private
+
+  def subtotal
+    @number_of_phones * @price
   end
 end
